f57a842520eb63b4dcc5632a96ad9e136e624f46
labl <zuobal@gmail.com>
2019-09-06 11:07:01 +0200

    --

--- plot_tracer_vint_maps.py
+++ plot_tracer_vint_maps.py
@@ -1,3 +1,3 @@
 from various_utils import tracer_vert_int
 
-tracer_vert_int('/home/labl/Bureau/XA54b.1.SEP05.003_SVT_CLD.nc','SVT001','05')
+tracer_vert_int('/home/labl/Bureau/XA54b.1.SEP05.003_SVT_CLD_DST.nc','SVT001','05', '/home/labl/Bureau/TEST0_ALT_dia_file.nc')
